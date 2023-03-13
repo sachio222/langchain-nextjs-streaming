65d74c1803253b3f19570f70915da6e5e8501279
Jake Krajewski <sachio222@users.noreply.github.com>
2023-03-12 22:53:06 -0400

    first commit

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Chatbot from "../components/Chatbot";
+
+function Home() {
+  return (
+    <div>
+      <h1>Minimal streaming chatbot example!</h1>
+      <Chatbot />
+    </div>
+  );
+}
+
+export default Home;
