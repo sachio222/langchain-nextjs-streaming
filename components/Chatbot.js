6c02e00da2ec97670bf6b6016da55e81c3e61e13
Jake Krajewski <sachio222@users.noreply.github.com>
2023-03-12 22:54:12 -0400

    first

---
+++ components/Chatbot.js
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+function Chatbot() {
+  const [chatMessages, setChatMessages] = useState([]);
+
+  const handleButtonClick = async () => {
+    setChatMessages([]);
+
+    const response = await fetch("/api/streamDemo");
+    const stream = response.body;
+    const reader = stream.getReader();
+
+    try {
+      while (true) {
+        const { done, value } = await reader.read();
+        if (done) {
+          break;
+        }
+        const decodedValue = new TextDecoder().decode(value);
+        setChatMessages((prevMessages) => [...prevMessages, decodedValue]);
+      }
+    } catch (error) {
+      console.error(error);
+    } finally {
+      reader.releaseLock();
+    }
+  };
+
+  return (
+    <>
+      <button
+        className="text-lg font-bold text-white"
+        onClick={handleButtonClick}
+      >
+        Start Stream
+      </button>
+      <ul>{chatMessages && chatMessages}</ul>
+    </>
+  );
+}
+
+export default Chatbot;
